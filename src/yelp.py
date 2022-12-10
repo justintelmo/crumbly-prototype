4e85f3273c5fec36963390f938bd2fec6da6fa88
Justin Telmo <justin.telmo@gmail.com>
2022-12-09 22:51:04 -0800

    Debugging response from Yelp API

--- src/yelp.py
+++ src/yelp.py
@@ -7,14 +7,17 @@ client = YelpAPI(api_key)
     
 def search_yelp(restaurant_name): 
     response = client.search_query(term=restaurant_name, location='fremont, ca', sort_by='rating', limit=5)
-    business = response['businesses'][0]
-    rating = business['rating']
-    review_count = business['review_count']
-    reviews = client.reviews(business['id'])
-
+    # business = response['businesses'][0]
+    # rating = business['rating']
+    # review_count = business['review_count']
+    # reviews = client.reviews(business['id'])
+    
     return {
-        'name': business['name'],
-        'rating': rating,
-        'review_count': review_count,
-        'reviews': [{'user': review['user']['name'], 'text': review['text']} for review in reviews['reviews']]
+        'response': response
     }
+    # return {
+    #     'name': business['name'],
+    #     'rating': rating,
+    #     'review_count': review_count,
+    #     'reviews': [{'user': review['user']['name'], 'text': review['text']} for review in reviews['reviews']]
+    # }
