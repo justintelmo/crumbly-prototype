e9ba3ae7ff8e0db5a0d47ce05bfa8d4d0354ead6
Justin Telmo <justin.telmo@gmail.com>
2022-12-09 23:34:54 -0800

    Only show reviews

--- src/yelp.py
+++ src/yelp.py
@@ -6,20 +6,9 @@ api_key = os.environ.get('YELP_API_KEY', None)
 client = YelpAPI(api_key)
     
 def search_yelp(restaurant_name): 
-    response = client.search_query(term=restaurant_name, location='fremont, ca', sort_by='rating', limit=5)
-    business = response['businesses'][0]
-    rating = business['rating']
-    review_count = business['review_count']
-    reviews = client.reviews_query(business['id'])
+    reviews = client.reviews_query(restaurant_name)
     
     
     return {
-        'business': business,
         'reviews': reviews['reviews']
     }
-    # return {
-    #     'name': business['name'],
-    #     'rating': rating,
-    #     'review_count': review_count,
-    #     'reviews': [{'user': review['user']['name'], 'text': review['text']} for review in reviews['reviews']]
-    # }
