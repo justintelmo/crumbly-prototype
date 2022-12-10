64c7ff128ddfc5b214a1f6a116b0365b97d5baa6
Justin Telmo <justin.telmo@gmail.com>
2022-12-10 00:01:18 -0800

    Show business and reviews simultaneously

--- src/yelp.py
+++ src/yelp.py
@@ -6,9 +6,18 @@ api_key = os.environ.get('YELP_API_KEY', None)
 client = YelpAPI(api_key)
     
 def search_yelp(restaurant_name): 
+    response = client.search_query(term=restaurant_name, location='fremont, ca', sort_by='rating', limit=5)
+    business = response['businesses'][0]
     reviews = client.reviews_query(restaurant_name)
     
     
     return {
+        'business': business,
         'reviews': reviews['reviews']
     }
+    # return {
+    #     'name': business['name'],
+    #     'rating': rating,
+    #     'review_count': review_count,
+    #     'reviews': [{'user': review['user']['name'], 'text': review['text']} for review in reviews['reviews']]
+    # }
